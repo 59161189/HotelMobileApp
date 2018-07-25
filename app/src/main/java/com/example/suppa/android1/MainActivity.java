e04789506c1c8156ca2bb25dcc8eaf07e5b214d4
59161189 <59161189@go.buu.ac.th>
2018-07-25 12:12:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/suppa/android1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.suppa.android1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton ibsg,ibtw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ibsg = (ImageButton) findViewById(R.id.single);
+        onStart();
+
+
+    }
+    protected void onStart(){
+        super.onStart();
+        ibsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,NewActivity.class));
+            }
+        });
+    }
+
+}
